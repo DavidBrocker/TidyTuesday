de30c05bf686b38bbb8a62f87baeafc06cc2719f
David Brocker <davidabrocker@gmail.com>
2020-10-08 10:20:58 -0400

    Better

--- 10-07.R
+++ 10-07.R
@@ -8,97 +8,58 @@ library(ggthemes)
 library(emoG)
 bball <- tidytuesdayR::tt_load('2020-10-06')
 
-
 bball <- bball$tournament
 
+
+bball_pal <- c("#FFFFFF","#A4C8E1")
+
 bball %>% 
+  mutate(First_Game_Home=x1st_game_at_home,
+         First_Game_Home=ifelse(First_Game_Home=="Y","Yes","No")) %>% 
   filter(tourney_finish=="Champ") %>% 
-  group_by(school,year) %>% 
-  ggplot(aes(school,conference))
-
-bball %>% 
-  mutate(pts = case_when(
-    seed==1~100,
-    seed==2~72.7
-    seed==3~54.5
-    seed==4~48.5
-    seed==5~33.3
-    seed==6~33.3
-    seed==7~27.3
-    seed==8~18.2
-    seed==9~18.2
-    seed==10~18.2
-    seed==11~18.2
-    seed==12~15.2
-    seed==13~9.09
-    seed==14~6.06
-    seed==15~3.03
-    seed==16~0
-  ))
-
-
-
-bball %>% 
-  mutate(home_game=
-           ifelse(x1st_game_at_home=="Y",1,0)
-         ) %>% 
-  filter(tourney_finish=="Champ") %>% 
-  select(school,home_game,tourney_finish) %>% 
-  group_by(school,home_game) %>% 
+  group_by(school,First_Game_Home) %>% 
   count(sort=T) %>% 
-  ggplot(aes(fct_reorder(school,n),n,fill=home_game)) +
-    geom_bar(stat="identity")+
-  coord_flip()
-
-bball %>% 
-  filter(tourney_finish=="Champ") %>% 
-  group_by(school) %>% 
-  count(sort=T)
-
-
-
-
-
-conn <- bball %>% 
-  filter(school=="UConn")
-
-conn$label <- rep(emoji("basketball"),30)
-
-conn %>%   
-ggplot(aes(year,tourney_w,label=label)) +
-  geom_hline(yintercept=6,linetype=2,color="green")+
-  geom_point()+
-  theme_clean()+
-  geom_text(family="OpenSansEmoji", 
-            size=5,color="orange")+
-  labs(title="UConn WNBA Tournament Outcomes\n\n",
+  ggplot(aes(fct_reorder(school,n),n,fill=First_Game_Home))+
+  geom_bar(stat="identity")+
+  coord_flip()+
+  labs(title="Tournaments Won",
+       subtitle="There's No Place Like Home!\n",
        x="\nTournament Wins",
-       y="\n\nYear",
+       y="Win Count",
        caption = "Visual: @DaveBrocker \n Source: @FiveThirtyEight")+
+  theme_clean()+
   theme(
-    panel.background = element_rect(fill="black"),
-    plot.background = element_rect(fill="black"),
+    panel.background = element_rect(fill="#000E2F"),
+    plot.background = element_rect(fill="#000E2F"),
     axis.text = element_text(color="white"),
-    plot.subtitle = element_text(color="white"),
+    plot.subtitle = element_text(color="white",size = 10),
     plot.title = element_text(color="white"),
     plot.caption = element_text(color="gold",face = "italic"),
     plot.title.position = "plot",
     plot.caption.position = "plot",
-    axis.title = element_text(color="white")
-  )
+    axis.title = element_text(color="white"),
+    legend.background = element_rect(fill="#000E2F",color = NA),
+    legend.text = element_text(color="white"),
+    legend.title = element_text(color="white")
+  )+
+  guides(fill=guide_legend(title="First Game at Home?"))+
+  scale_fill_manual(values=bball_pal)
 
-idk <- paste0(conn$year,"-01-01")
 
-as.Date(idk,format = "%y")
-
+ggsave("wnba.jpeg",device="jpeg")
 
 
-idkset.seed(123)
-x <- rnorm(10)
-set.seed(321)
-y <- rnorm(10)
-plot(x, y, cex=0)
-text(x, y, labels=emoji('basketball'), cex=1.5, col='orange', family='OpenSansEmoji')
+
+
+
+
+
+
+
+
+
+
+
 
 
 
